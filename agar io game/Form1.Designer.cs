a64e9bc26765df37299c55cd054545a187abf085
Mark Olah <114992682+OlahMark19@users.noreply.github.com>
2023-06-02 13:42:19 +0300

    Add project files.

---
+++ agar io game/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace agar_io_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ItmCount = new System.Windows.Forms.Label();
+            this.moveTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.player.Location = new System.Drawing.Point(768, 293);
+            this.player.Margin = new System.Windows.Forms.Padding(2);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(30, 30);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // ItmCount
+            // 
+            this.ItmCount.AutoSize = true;
+            this.ItmCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ItmCount.Location = new System.Drawing.Point(27, 23);
+            this.ItmCount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.ItmCount.Name = "ItmCount";
+            this.ItmCount.Size = new System.Drawing.Size(98, 26);
+            this.ItmCount.TabIndex = 1;
+            this.ItmCount.Text = "Items: 0";
+            // 
+            // moveTimer
+            // 
+            this.moveTimer.Enabled = true;
+            this.moveTimer.Interval = 20;
+            this.moveTimer.Tick += new System.EventHandler(this.moveTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1426, 839);
+            this.Controls.Add(this.ItmCount);
+            this.Controls.Add(this.player);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Screen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label ItmCount;
+        private System.Windows.Forms.Timer moveTimer;
+    }
+}
+
---
+++ agar io game/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace agar_io_game
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft, goRight, goUp, goDown;
+        int playerSpeed = 5;
+        int playerSize = 20;
+        int items = 0;
+
+        private Random random = new Random();
+        private List<PictureBox> pictureBoxes = new List<PictureBox>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            GenerateRandomPictureBoxes(75); // Generate 10 random PictureBoxes on form load
+        }
+        private void GenerateRandomPictureBoxes(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.Size = new Size(10, 10); // Set the size of the PictureBox
+                pictureBox.Location = GenerateRandomLocation(); // Generate random location
+
+                // Set a random background color
+                pictureBox.BackColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+
+                Controls.Add(pictureBox); // Add PictureBox to the form's Controls collection
+                pictureBoxes.Add(pictureBox); // Add PictureBox to the list of PictureBoxes
+            }
+        }
+
+        private Point GenerateRandomLocation()
+        {
+            int x = random.Next(ClientSize.Width ); // Subtracting 100 to keep PictureBox within form boundaries
+            int y = random.Next(ClientSize.Height ); // Subtracting 100 to keep PictureBox within form boundaries
+            return new Point(x, y);
+        }
+
+         private void moveTimerEvent(object sender, EventArgs e)
+        {
+            ItmCount.Text = "Items: " + items;
+        
+            if (goLeft == true && player.Left > 0)
+            {
+                player.Left -= playerSpeed;
+            }
+            if (goRight == true && player.Left < 1395)
+            {
+                player.Left += playerSpeed;
+            }
+            if (goUp == true && player.Top > 0)
+            {
+                player.Top -= playerSpeed;
+            }
+            if (goDown == true && player.Top < 810)
+            {
+                player.Top += playerSpeed;
+            }
+
+            CheckCollision();
+        }
+        private void CheckCollision()
+        {
+            foreach (PictureBox pictureBox in pictureBoxes)
+            {
+                if (player.Bounds.IntersectsWith(pictureBox.Bounds))
+                {
+                    playerSize += 5; // Increase player's size
+                    player.Size = new Size(playerSize, playerSize);
+                    items++;
+
+                    pictureBox.Hide(); // Hide the colliding PictureBox
+                    pictureBoxes.Remove(pictureBox); // Remove the PictureBox from the list
+                    Controls.Remove(pictureBox); // Remove the PictureBox from the form's Controls collection
+                    break; // Exit the loop after handling one collision
+                }
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+        }
+
+    }
+}
